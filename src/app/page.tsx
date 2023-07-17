f395402259890491b8386185d248a4bd89f32f20
Gabriel Borges <gabriel.borges_96@hotmail.com>
2023-07-17 11:18:51 -0300

    feat: project initializaded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,164 @@
-import Image from 'next/image'
+"use client"
+
+import { styled } from "styled-components"
+
+const Main = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  padding-bottom: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: var(--primary-color-full);
+  padding-bottom: 30px;
+`;
+
+const Div = styled.div`
+  width: 370px;
+`;
+
+
+const TextLabel = styled.p`
+  position: absolute;
+  top: -10px;
+  left: 20px;
+  padding: 0 5px;
+  background-color: #0F172A;
+`;
+
+const Label = styled.label`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  color: var(--primary-color);
+
+  &:focus-within ${TextLabel} {
+    color: var(--primary-color-full); 
+  }
+
+  &:first-child {
+    margin-bottom: 60px;
+  }
+`;
+
+
+const Input = styled.input`
+  padding: 15px 13px 13px 13px;
+  font-size: 16px;
+  color: var(--primary-color);
+  background-color: transparent;
+  border: 1px var(--primary-color) solid;
+  border-radius: 10px;
+
+  &::placeholder {
+      color: var(--secondary-color);
+  }
+`;
+
+const Forgot = styled.a`
+  display: flex;
+  justify-content: flex-end;
+  text-decoration: underline;
+  font-size: 12px;
+  color: var(--primary-color);
+  transition: all .3s ease-in-out;
+
+  &:hover {
+    color: var(--primary-color-full);
+  }
+`;
+
+const DivButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 30px;
+`;
+
+const FirstButton = styled.button`
+  background-color: var(--secondary-color-half);
+  padding: 9px 33px 7px 33px;
+  border-radius: 10px;
+  border: solid 1px transparent;
+  color: var(--primary-color);
+  font-size: 16px;
+  cursor: pointer;
+  transition: all .3s ease-in-out;
+
+  &:hover {
+    color: var(--primary-color-full);
+    background-color: var(--secondary-color-minimum);
+  }
+`;
+
+const SeccondButton = styled.button`
+  background-color: transparent;
+  padding: 9px 33px 7px 33px;
+  border-radius: 10px;
+  border: solid 1px var(--secondary-color-half);
+  color: var(--secondary-color);
+  font-size: 16px;
+  cursor: pointer;
+  transition: all .3s ease-in-out;
+
+  &:hover {
+    color: var(--primary-color-full);
+    background-color: var(--secondary-color-half);
+  }
+`;
 
 export default function Home() {
+
+  // const navigate = useNavigate();
+
+  // const handleEntrarClick = () => {
+  //   // Realize as ações necessárias antes de navegar para a próxima página
+  //   // ...
+
+  //   // Navegue para a próxima página
+  //   navigate("/outra-pagina");
+  // };
+
+  // const handleCriarContaClick = () => {
+  //   // Realize as ações necessárias antes de navegar para a próxima página
+  //   // ...
+
+  //   // Navegue para a próxima página
+  //   navigate("/outra-pagina");
+  // };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Main>
+      <Image src='./logo.png' alt="" />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Title>Sussuros do Baralho</Title>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <Label>
+          <TextLabel>E-mail</TextLabel>
+          <Input type="text" placeholder="Digite seu e-mail"/>
+        </Label>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <Label>
+        <TextLabel>Senha</TextLabel>
+          <Input type="password" placeholder="Digite sua senha"/>
+        </Label>
+        <Forgot href="#">Esqueceu sua senha?</Forgot>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <DivButton>
+          <FirstButton >Entrar</FirstButton>
+          <SeccondButton >Criar conta</SeccondButton>
+        </DivButton>
+      </Div>
+    </Main>
   )
 }
